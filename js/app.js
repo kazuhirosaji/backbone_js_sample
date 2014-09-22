453b25b6be2e7799d4c23c73c762c856257e8962
kazuhirosaji <kazuhirosaji@gmail.com>
2014-09-22 09:19:48 -0700

    extend model test

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 	console.log("Hello Backbone!");
 
 	modelTest();
+	extendModelTest();
 
 }())
 
@@ -16,4 +17,26 @@ function modelTest() {
 
 	var obj2 = new Backbone.Model({name: "Kenichiro", age: 30});
 	console.log("obj2: " + JSON.stringify(obj));	
+}
+
+function extendModelTest() {
+	var Staff = Backbone.Model.extend({
+		defaults: {
+			"name": "",
+			"age" : 0,
+			"updateTime": new Date
+		},
+		initialize: function() {
+			console.log("Staff[" + this.cid + "]: " + JSON.stringify(this));
+		}
+	});
+
+	console.log("========= extendModelTest start =========");
+	var tmpStaff = new Staff();
+	tmpStaff.set({name: "Murata", age: 15, id: 101});
+
+	console.log("Staff[" + tmpStaff.cid +"]: " + JSON.stringify(tmpStaff));
+
+	var tmpStaff2 = new Staff({name: "Kenichiro", age: 35, id: 102});
+	console.log("========= extendModelTest end =========");
 }

a0ec326232645e2ec4bc58ff7fc554333b472c7c
kazuhirosaji <kazuhirosaji@gmail.com>
2014-09-22 22:13:34 -0700

    collection extend

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 	modelTest();
 	extendModelTest();
 	collectionTest();
+	extendCollectionTest();
 
 }())
 
@@ -84,3 +85,31 @@ function collectionTest() {
 	console.log("========= collectionTest end =========");	
 }
 
+function extendCollectionTest() {
+	var Staff = Backbone.Model.extend({
+		defaults: {
+			"name": "",
+			"age" : 0,
+			"updateTime": new Date
+		},
+		initialize: function() {
+			console.log("Staff[" + this.cid + "]: " + JSON.stringify(this));
+		}
+	});
+
+	console.log("========= extendCollectionTest start =========");
+	var tmpStaff = new Staff();
+	tmpStaff.set({name: "Murata", age: 15, id: 101});
+	var tmpStaff2 = new Staff({name: "Kenichiro", age: 35, id: 102});	
+
+	var Staffs = Backbone.Collection.extend({
+		model: Staff
+	});
+	var staffs = new Staffs([tmpStaff, tmpStaff2]);
+
+	console.log("staffs: " + JSON.stringify(staffs));
+	console.log("staffs.get(cid): " + JSON.stringify(staffs.get("c9")));
+	console.log("staffs.at(index): " + JSON.stringify(staffs.at(1)));
+	console.log("staffs.get(id): " + JSON.stringify(staffs.get(102)));
+	console.log("========= extendCollectionTest end =========");
+}
